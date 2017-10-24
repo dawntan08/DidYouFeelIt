ac5b5ad7b98eb3a8796bde7e591c45bca4baad53
Dawn <dawntan08@gmail.com>
2017-10-24 11:45:00 +1100

    Move the network request to asynctask to fix the crash

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,11 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        // make and create EarthquakAsyncTask to handle fetching earthquake data
+        new EarthquakeAsyncTask().execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +51,20 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event>{
+        // perform the http request in the background
+        protected Event doInBackground(String... urls){
+            // Perform the HTTP request for earthquake data and process the response.
+            Event result = Utils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+
+        // update the ui with the data taken from the site
+        protected void onPostExecute(Event result){
+            // Update the information displayed to the user.
+            updateUi(result);
+        }
     }
 }
